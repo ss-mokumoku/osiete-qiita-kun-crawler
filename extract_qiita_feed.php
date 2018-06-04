78ff2a5707b334ee8f09e5280b050880f671b525
arita-takahiro <arita-takahiro@systemsoft.co.jp>
2018-06-04 17:14:26 +0900

    インデントの修正

--- extract_qiita_feed.php
+++ extract_qiita_feed.php
@@ -1,44 +1,47 @@
 <?php
+
 //Qiitaの人気記事のRSSを取得するコードです
 
-function extract_qiita_feed($file){
-
+function extract_qiita_feed($file)
+{
     $xmlstr = file_get_contents($file);
     $feed = new SimpleXMLElement($xmlstr);
 
-    $feedtitle   =(string)$feed->title;
-    $feedupdated =(string)$feed->updated;
-    $contents = array();
+    $feedtitle = (string) $feed->title;
+    $feedupdated = (string) $feed->updated;
+    $contents = [];
 
-//タイトル、著者、投稿日時、更新日時、リンク、item_idをRSSから抽出します
-    foreach($feed->entry as $entry){
-        $published = DateTime::createFromFormat(DateTime::ATOM,(string)$entry->published);
-        $updated = DateTime::createFromFormat(DateTime::ATOM,(string)$entry->updated);
-        $link = (string)$entry->link['href'];
+    //タイトル、著者、投稿日時、更新日時、リンク、item_idをRSSから抽出します
+    foreach ($feed->entry as $entry) {
+        $published = DateTime::createFromFormat(DateTime::ATOM, (string) $entry->published);
+        $updated = DateTime::createFromFormat(DateTime::ATOM, (string) $entry->updated);
+        $link = (string) $entry->link['href'];
         $link = urldecode($link);
         $url = parse_url($link);
         $item = basename($url['path']);
 
-        $entrydata = array(
-            "title"     => (string)$entry->title,
-            "author"    => (string)$entry->author->name,
-            "published" => $published,
-            "updated"   => $updated,
-            "link"      => $link,
-            "item_id"   => $item,
-        );
+        $entrydata = [
+            'title' => (string) $entry->title,
+            'author' => (string) $entry->author->name,
+            'published' => $published,
+            'updated' => $updated,
+            'link' => $link,
+            'item_id' => $item,
+        ];
 
         $contents[] = $entrydata;
-
     }
     //RSSのタイトル「Qiita - 人気の投稿」とRSSの更新日時と記事ごとのデータをリターンします
-    return array("title" => $feedtitle,"updated" => $feedupdated,"contents" => $contents);
+    return ['title' => $feedtitle, 'updated' => $feedupdated, 'contents' => $contents];
 }
 
 /**
- * データを抽出し、画面に表示する
+ * データを抽出し、画面に表示する.
+ *
+ * @param mixed $filepath
  */
-function extract_and_print($filepath) {
+function extract_and_print($filepath)
+{
     $data = extract_qiita_feed($filepath);
     myprint($data);
 }
--- qiita_json.php
+++ qiita_json.php
@@ -1,60 +1,55 @@
 <?php
-function qiita_json($item){
 
-$c = curl_init('https://qiita.com/api/v2/items/'.$item);
-//$c = curl_init('http://10.20.30.99/qiita/'.$item.'.json');
+function qiita_json($item)
+{
+    $c = curl_init('https://qiita.com/api/v2/items/'.$item);
+    //$c = curl_init('http://10.20.30.99/qiita/'.$item.'.json');
 
-curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-$json = curl_exec($c);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+    $json = curl_exec($c);
 
-//文字化けをしないようにする
-$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-//連想配列にする
+    //文字化けをしないようにする
+    $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+    //連想配列にする
 
-$arr = json_decode($json,true);
-$json_count = count($arr['tags']);
-$tags_count = array();
+    $arr = json_decode($json, true);
+    $json_count = count($arr['tags']);
+    $tags_count = [];
 
-
-    for($i=$json_count-1;$i>=0;$i--){
+    for ($i = $json_count - 1; $i >= 0; --$i) {
         $tag = $arr['tags'][$i]['name'];
         $tags_count[] = $tag;
     }
 
-$contents = array(
-//authors_tblの項目
-    "permanent_id" => $arr['user']['permanent_id'],
-    "user_id"      => $arr['user']['id'],
-    "profile_image_url"        => $arr['user']['profile_image_url'],
-    "description"  => $arr['user']['description'],
-    "location"     => $arr['user']['location'],
-    "organization" => $arr['user']['organization'],
-    "followees_count"=>$arr['user']['followees_count'],
-    "followers_count"=>$arr['user']['followers_count'],
-    "items_count"     => $arr['user']['items_count'],
-    "github_login_name"=>$arr['user']['github_login_name'],
-    "linkedin_id"=>$arr['user']['linkedin_id'],
-    "facebook_id"=>$arr['user']['facebook_id'],
-    "twitter_screen_name"=>$arr['user']['twitter_screen_name'],
-    "website_url"=>$arr['user']['website_url'],
+    return [
+        //authors_tblの項目
+        'permanent_id' => $arr['user']['permanent_id'],
+        'user_id' => $arr['user']['id'],
+        'profile_image_url' => $arr['user']['profile_image_url'],
+        'description' => $arr['user']['description'],
+        'location' => $arr['user']['location'],
+        'organization' => $arr['user']['organization'],
+        'followees_count' => $arr['user']['followees_count'],
+        'followers_count' => $arr['user']['followers_count'],
+        'items_count' => $arr['user']['items_count'],
+        'github_login_name' => $arr['user']['github_login_name'],
+        'linkedin_id' => $arr['user']['linkedin_id'],
+        'facebook_id' => $arr['user']['facebook_id'],
+        'twitter_screen_name' => $arr['user']['twitter_screen_name'],
+        'website_url' => $arr['user']['website_url'],
 
-//articles_tblの項目
-    "post_id"      => $arr['id'],
-    "body" => $arr['body'],
-    "private"=> $arr['private'],
-    "page_views_count"=> $arr['page_views_count'],
-    "likes_count"  => $arr['likes_count'],
-    "comments_count"=>$arr['comments_count'],
-    "reactions_count"=>$arr['reactions_count'],
-    "coediting"=>$arr['coediting'],
-    "tags"         => $tags_count,
-);
-return $contents;
+        //articles_tblの項目
+        'post_id' => $arr['id'],
+        'body' => $arr['body'],
+        'private' => $arr['private'],
+        'page_views_count' => $arr['page_views_count'],
+        'likes_count' => $arr['likes_count'],
+        'comments_count' => $arr['comments_count'],
+        'reactions_count' => $arr['reactions_count'],
+        'coediting' => $arr['coediting'],
+        'tags' => $tags_count,
+    ];
 }
-
-
-
-
 
 /*
 $json = file_get_contents("qiita_API.json");
@@ -66,4 +61,3 @@ echo $arr['id'];
 echo $arr['likes_count'];
 echo $arr['user']['profile_image_url'];
 */
-?>
